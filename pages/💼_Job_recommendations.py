89e4a6809b9dce98e2f69ab012cfdee86620d423
Vengatesan K <128688827+Vengatesan-K@users.noreply.github.com>
2023-09-23 15:19:48 +0530

    Update 💼_Job_recommendations.py

--- pages/💼_Job_recommendations.py
+++ pages/💼_Job_recommendations.py
@@ -27,7 +27,7 @@ job_keywords = st.text_input("Job Keywords", "Data Scientist")
 
 if st.button("Scrape Jobs"):
   url1 = f'https://www.linkedin.com/jobs/search?keywords={job_keywords}&location={location}&trk=public_jobs_jobs-search-bar_search-submit'
-  @st.experimental_singleton
+  @st.cache_resource
   def installff():
     os.system('sbase install geckodriver')
     os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
