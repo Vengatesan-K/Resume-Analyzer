9d13227bb893058839859caa082c0646b7a527d6
Vengatesan K <128688827+Vengatesan-K@users.noreply.github.com>
2023-09-23 15:29:54 +0530

    Update 💼_Job_recommendations.py

--- pages/💼_Job_recommendations.py
+++ pages/💼_Job_recommendations.py
@@ -22,7 +22,7 @@ colored_header(
     color_name="red-70",
 )
 @st.cache_resource
-  def installff():
+def installff():
     os.system('sbase install geckodriver')
     os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
 
